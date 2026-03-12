0717f22a20ffb155cc752a5231b38e2bc4c910fd
NotknowEAC <139251675+notknowEAC@users.noreply.github.com>
2026-03-12 17:11:52 +0700

    update javaFX

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,13 +1,25 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello" );
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        Button btn = new Button("Hello JavaFX");
+
+        Scene scene = new Scene(btn, 400, 300);
+
+        stage.setTitle("My JavaFX App");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
