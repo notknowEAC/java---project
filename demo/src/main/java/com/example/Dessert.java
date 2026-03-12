45f2ab8b545c3e8278934df0eb6f771d0716ec1a
NotknowEAC <139251675+notknowEAC@users.noreply.github.com>
2026-03-12 16:50:33 +0700

    update menu

---
+++ demo/src/main/java/com/example/Dessert.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Dessert extends Menu{
+    
+    public Dessert(String name,int price){
+        super(name, price);
+        this.name=name;
+        this.price=price;
+    }
+}
---
+++ demo/src/main/java/com/example/Size.java
@@ -0,0 +1,6 @@
+package com.example;
+
+public class Size {
+    public String[] size = {"S","M","L"};
+    public int[] price = {50,60,70};
+}
