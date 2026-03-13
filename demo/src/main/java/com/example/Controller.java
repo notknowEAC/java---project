264b3f882683fa6083e9398f8a1cc65ef0ac1b12
NotknowEAC <139251675+notknowEAC@users.noreply.github.com>
2026-03-13 13:21:52 +0700

    update ui

---
+++ demo/src/main/java/com/example/Controller.java
@@ -0,0 +1,13 @@
+package com.example;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+public class Controller {
+    @FXML
+    private TextField tiTitle;
+
+    @FXML
+    void btnOKClicked(ActionEvent event) {
+
+    }
+}
